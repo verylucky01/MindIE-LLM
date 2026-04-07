498a7c0f24d542fda82941e8d75fbfafc9a7efaa
earthmanylf <yulinfeng2@huawei.com>
2026-04-07 16:32:10 +0800

    warmup的inputid改为随机数
    Co-authored-by: earthmanylf<yulinfeng2@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !779 merge random into dev
    
    warmup的inputid改为随机数
    
    Created-by: earthmanylf
    Commit-by: earthmanylf
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20251225
    -->
    
    # 合入背景
    > 请描述为什么要做这个PR内的改动。\
    > 如涉及，请关联前序PR或同特性/需求下的其他PR。\
    > 如果是修复之前PR引入的问题，请关联引入问题的PR。\
    > 注意：`Fixes #ISSUE ID`会自动关闭issue，如问题部分解决请不要使用`Fixes`，可以用`Fix part of #ISSUE ID`替代.
    
    Fix part of [#103](https://gitcode.com/Ascend/MindIE-LLM/issues/103) 
    
    # 修改内容
    > 请描述修改内容的具体实现，涉及哪些组件之间进行交互，可以用1、2、3、...进行罗列。\
    > 如果是需求或者重构类的PR，需要补充详细设计文档（说明上下游组件关系、时序图、类图、DFX能力等内容）。
    
    warmup构造的input_id从全1修改为随机数
    
    # 资料变更
    > 请确认是否涉及资料变更。如涉及，需要在PR中体现，并简要说明修改内容。如不涉及，需填写“不涉及”。
    
    不涉及
    
    # 接口变更
    > 请确认是否涉及跨代码仓或者客户面可见的接口变更。如涉及，需要详细说明接口以及对应的变更内容，同时需要在资料中体现。如不涉及，需填写“不涉及”。
    
    不涉及
    
    # 测试结果
    > 请说明测试场景，测试方法以及测试结果。\
    > 测试用例设计时需考虑硬件、部署方式、功能、性能、精度、显存等维度。
    
    dsv32：
    curl http://localhost:3025/v1/chat/completions -X POST -d'{"model": "torch","messages": [{"role": "user","content": "You are a helpful assistant."}],"stream": true,"temperature": 0,"max_tokens": 10}'
    data: {"id":"endpoint_common_1","object":"chat.completion.chunk","created":1775545382,"model":"torch","choices":[{"index":0,"delta":{"role":"assistant","content":""},"logprobs":null,"finish_reason":null}]}
    
    data: {"id":"endpoint_common_1","object":"chat.completion.chunk","created":1775545382,"model":"torch","choices":[{"index":0,"delta":{"role":"assistant","content":"Hello"},"logprobs":null,"finish_reason":null}]}
    
    data: {"id":"endpoint_common_1","object":"chat.completion.chunk","created":1775545382,"model":"torch","choices":[{"index":0,"delta":{"role":"assistant","content":"! I'm"},"logprobs":null,"finish_reason":null}]}
    
    qwen2.5-vl：
    curl http://localhost:3025/generate -X POST -d'{"prompt": "My name is Olivier and I", "max_tokens": 20, "temperature": 0, "stream": false, "model": "None"}'
    {"text":["My name is Olivier and I 'm a 25-year-old Frenchman. I have been living in the UK for 3"]}
    
    # CheckList
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]。
    
    - [x] 代码注释完备
    - [x] 正确记录错误日志
    - [x] 进行了返回值校验 (禁止使用void屏蔽安全函数、自研函数返回值；考虑接口的异常场景；调用底层组件接口时，需要进行返回值校验)
    - [x] 进行了空指针校验
    - [x] 若存在资源申请，使用后资源被正确的释放了
    - [x] 若涉及多线程场景，考虑了并发场景，不存在死锁问题
    - [x] 按照[代码仓中提供的格式模板](https://gitcode.com/Ascend/MindIE-LLM/blob/master/.clang-format)，使用clang-format工具格式化代码
    - [x] 符合Ascend社区的编码规范。[C++ 语言编程指导](https://gitcode.com/Ascend/community/blob/master/docs/contributor/Ascend-cpp-coding-style-guide.md) | [C++ 语言安全编程指导](https://gitcode.com/Ascend/community/blob/master/docs/contributor/Ascend-cpp-secure-coding-guide.md)
    
    
    See merge request: Ascend/MindIE-LLM!779

--- mindie_llm/text_generator/generator.py
+++ mindie_llm/text_generator/generator.py
@@ -961,7 +961,9 @@ class Generator(PDInterface):
                     max_output_len=max_output_len,
                     max_placeholder_num=max_placeholder_num,
                     warmup_topk_size=self.warmup_topk_size,
-                    enable_warmup_sampling=self.enable_warmup_with_sampling
+                    enable_warmup_sampling=self.enable_warmup_with_sampling,
+                    vocab_size=self.vocab_size,
+                    is_multimodal=self.is_multimodal
                 )
                 request.build(
                     dp_rank_id=dp_idx,
